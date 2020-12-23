8d94735adffbb0e4502de2a421817189c6c11602
jgersain <jhersein@gmail.com>
2020-12-23 16:29:17 -0600

    Add vue router and home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <main></main>
+  <main>
+    <px-header />
+    <router-view class="container px-5 sm:px-20 py:px-20 flex justify-center" />
+  </main>
 </template>
 
 <script>
+import PxHeader from '@/components/PxHeader'
 
 export default {
   name: "App",
   components: {
+    PxHeader,
   }
 };
 </script>
