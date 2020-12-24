57f0263f49cb1af700f80b38d9c6ea4c15e0e871
jgersain <jhersein@gmail.com>
2020-12-23 21:25:56 -0600

    Add a new route for coin detail

--- src/api.js
+++ src/api.js
@@ -1,12 +1,20 @@
 const url = 'https://api.coincap.io/v2'
 
-// get assets
+// get 20 main assets
 function getAssets() {
   return fetch(`${url}/assets?limit=20`)
     .then(res => res.json())
     .then(res => res.data)
 }
 
+// get an asset
+function getAsset(coin) {
+  return fetch(`${url}/assets/${coin}`)
+    .then(res => res.json())
+    .then(res => res.data)
+}
+
 export default {
-  getAssets
+  getAssets,
+  getAsset
 }
